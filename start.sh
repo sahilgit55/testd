19c41cef9e8c08a739d7472386961e587ffaeb13
sahilgit55 <113678294+sahilgit55@users.noreply.github.com>
2023-02-08 21:53:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
+git clone https://github.com/sahilgit55/VideoFlux1.0 ok && cd ok && pip3 install -U -r requirements.txt
 $START_CMD
